135bbbfacadfd5f03810bade4340aacef96a26a2
Thomas Jeon <thomasjeon@thomass-mbp.home>
2014-11-03 22:57:20 -0500

    change callback url for heroku

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
   'googleAuth' : {
     'clientID'        : '68199768281-0qjamjidun2r29jmbingfufep6jbt6ji.apps.googleusercontent.com',
     'clientSecret'    : '0vZKvtMyw4xtdSSxr04ivOQW',
-    'callbackURL'     : 'http://localhost:8080/auth/google/callback'
+    'callbackURL'     : 'http://dodotodo.herokuapp.com/auth/google/callback'
   }
 };
